fbdae31641169767e05bb86d548831124311b64b
busitora <ts12272530@gmail.com>
2019-11-02 16:31:37 +0900

    [fix]task_specの修正(

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :task do
-    title {"テストタイトル"}
-    content {"テストコンテント"}
+    sequence(:title) {|n|"テストタイトル#{n}"}
+    sequence(:content) {|n|"テストコンテント#{n}"}
     status {:todo}
     user_id {1}
   end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,49 +1,46 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
 
-  let(:user) {FactoryBot.create(:user)}
-  # let で インスタンス変数のように定義し、FBでデータを作成
-  describe "タイトルのバリデーションに関するテスト" do
-    context "タイトルが空の時" do
-      it "エラーになる" do
-        task = Task.new(
-          content: "テストコンテンツ",
-          user_id: user.id,
-          status: 0
-        )
-        expect(task).not_to be_valid
+  describe "バリデーションのテスト" do
+    let(:user) {create(:user)}
+    # let で インスタンス変数のように定義し、FBでデータを作成
+
+    # 正常系
+    context "バリデーションが全て正しい時" do
+      it "エラーにならない" do
+        task = build(:task)
+        # build が FB のメソッドでtasks.rbから:task を呼んでくる
+        expect(task).to be_valid
       end
     end
-    context "タイトルが重複している時" do
-      it "エラーになる" do
-        Task.create(
-          title: "テストタイトル",
-          content: "テストコンテンツ",
-          user_id: user.id,
-          status: 0
-        )
 
-        task = Task.new(
-          title: "テストタイトル",
-          content: "テストコンテンツ",
-          user_id: user.id,
-          status: 0
-        )
-        expect(task.valid?).to eq(false)
+    # 異常系
+    describe "タイトルのバリデーションに関するテスト" do
+      context "タイトルが空の時" do
+        it "エラーになる" do
+          task = build(:task, title: "")
+          # create ではなく build。 validation は create の前に走る。タイトルを空にしたケース
+          expect(task).not_to be_valid
+        end
+      end
+
+      context "タイトルが重複している時" do
+        it "エラーになる" do
+          task = create(:task)
+          # Factory.Botから tasks.rb を呼びtaskを登録
+          duplicated_task = build(:task, title: task.title)
+          expect(duplicated_task.valid?).to eq(false)
+        end
       end
     end
-  end
 
-  describe "ステータスのバリデーションに関するテスト" do
-    context "ステータスが空の時" do
-      it "エラーになる" do
-        task = Task.new(
-          content: "テストコンテンツ",
-          user_id: user.id,
-        )
-        expect(task).not_to be_valid
+    describe "ステータスのバリデーションに関するテスト" do
+      context "ステータスが空の時" do
+        it "エラーになる" do
+          task = build(:task, status: "")
+          expect(task).not_to be_valid
+        end
       end
     end
   end
