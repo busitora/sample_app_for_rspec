606f41e3564e1236ea4a6b6e5663f8e35a7d6e19
busitora <ts12272530@gmail.com>
2019-11-03 14:22:40 +0900

    [add]user_sessions.spec.rbのテストケースの条件を設定した

--- spec/system/user_sessions_spec.rb
+++ spec/system/user_sessions_spec.rb
@@ -4,6 +4,27 @@ RSpec.describe "UserSessions", type: :system do
   before do
     driven_by(:rack_test)
   end
+  describe "ユーザーのログイン挙動テスト" do
 
-  pending "add some scenarios (or delete) #{__FILE__}"
+    describe "ログイン前のケース" do
+      # 正常系
+      context "フォーム入力値が全て正しい時" do
+        it "ログインが成功する" do
+        end
+      end
+      # 異常系
+      context "フォームが未入力の時" do
+        it "ログインが失敗する" do
+        end
+      end
+    end
+
+    describe "ログイン後のケース" do
+      # 正常系
+      context "ログアウトボタンをクリックした時" do
+        it "ログアウトに成功する" do
+        end
+      end
+    end
+  end
 end
