2f7e19397f624002de9ac73874bd8d00f701b6c5
busitora <ts12272530@gmail.com>
2019-11-05 14:02:24 +0900

    [add]taskのsystemスペックを書いた

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,8 +1,9 @@
 FactoryBot.define do
   factory :task do
     association :user
-    sequence(:title) {|n|"テストタイトル#{n}"}
-    sequence(:content) {|n|"テストコンテント#{n}"}
+    sequence(:title) {|n| "テストタイトル#{n}"}
+    sequence(:content) {|n| "テストコンテント#{n}"}
     status {:todo}
+    sequence(:deadline) {|n| "#{Date.current + n.days}"}
   end
 end
--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -4,12 +4,23 @@ RSpec.describe "Tasks", type: :system do
   before do
     driven_by(:rack_test)
   end
+
   describe "タスクの挙動テスト" do
+    let(:user) {create(:user)}
+    let(:other_user) {(create(:user))}
+    let(:task) {create(:task, user_id: user.id)}
+    let(:other_task) {create(:task, user_id: other_user.id)}
+
     describe "ログイン前のケース" do
       # 異常系
       describe "タスクの新規作成画面" do
         context "権限がないため" do
           it "アクセスに失敗する" do
+            user
+            visit new_task_path
+            expect(login_path).to eq(current_path)
+            expect(page).to have_content "Login required"
+            # ログインせずに新規作成は出来ずに、login画面へリダイレクト
           end
         end
       end
@@ -17,6 +28,11 @@ RSpec.describe "Tasks", type: :system do
       describe "タスクの編集画面" do
         context "権限がないため" do
           it "アクセスに失敗する" do
+            user
+            visit edit_task_path(user)
+            expect(login_path).to eq(current_path)
+            expect(page).to have_content "Login required"
+            # ログインせずに編集画面にはリダイレクト出来ずに、login画面へ
           end
         end
       end
@@ -27,6 +43,15 @@ RSpec.describe "Tasks", type: :system do
         # 正常系
         context "フォームの入力値が全て正しい時" do
           it "タスクの新規作成に成功する" do
+            login(user)
+            visit new_task_path
+            fill_in "Title", with: "最初のタスク"
+            fill_in "Content", with: "最初のコンテント"
+            select "todo", from: "Status"
+            click_button("Create Task")
+            # expect(task_path(task.id)).to eq(current_path)
+            # TODO この書き方だとletのtaskが呼ばれる。要修正
+            expect(page).to have_content "Task was successfully created."
           end
         end
       end
@@ -34,11 +59,26 @@ RSpec.describe "Tasks", type: :system do
         # 正常系
         context "フォームの入力値が全て正しい時" do
           it "タスクの編集に成功する" do
+            login(user)
+            visit edit_task_path(task)
+            fill_in "Title", with: "2つ目のタスク"
+            fill_in "Content", with: "2つめのタスクに変更"
+            select "doing", from: "Status"
+            fill_in "Deadline", with: '31 December 201'
+            click_button("Update Task")
+            expect(page).to have_content 'Task was successfully updated.'
           end
         end
         # 異常系
         context "他ユーザーの編集ページにアクセスした時" do
           it "権限がないためアクセスが失敗する" do
+            login(user)
+            other_task
+            # 他のタスクを作成
+            visit edit_task_path(other_task)
+            expect(root_path).to eq(current_path)
+            # 他人の編集ページにアクセスしたのでリダイレクト
+            expect(page).to have_content "Forbidden access."
           end
         end
       end
@@ -46,6 +86,11 @@ RSpec.describe "Tasks", type: :system do
         # 正常系
         context "Destroyボタンをクリックした時"
         it "タスクの削除に成功する" do
+          login(user)
+          task
+          click_link("Logout")
+          expect(page.driver.browser.switch_to.alert.text).to eq "Are you sure?"
+          expect(page).to have_content "Task was successfully destroyed."
         end
       end
     end
