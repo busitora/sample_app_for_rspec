e86c517f4fc5897290c6769f66f99454420ce3c7
busitora <ts12272530@gmail.com>
2019-11-05 18:05:33 +0900

    [add]テストをchrome起動するかしないかを設定した

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe "Tasks", type: :system do
-  before do
-    driven_by(:rack_test)
-  end
+  # before do
+  #   driven_by(:rack_test)
+  # end
 
   describe "タスクの挙動テスト" do
     let(:user) {create(:user)}
--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -1,9 +1,9 @@
 require "rails_helper"
 
 RSpec.describe "Users", type: :system do
-  before do
-    driven_by(:rack_test)
-  end
+  # before do
+  #   driven_by(:rack_test)
+  # end
   describe "ユーザーの挙動テスト" do
     let(:user) {create(:user)}
     let(:other_user) {(create(:user))}
