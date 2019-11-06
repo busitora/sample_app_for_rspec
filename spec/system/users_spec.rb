abf4605b76a7c4bb11ebd751910b13cb722b28c6
busitora <ts12272530@gmail.com>
2019-11-06 11:18:33 +0900

    [add]ユーザー編集したemailをマイページから確認できるようにした

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -77,6 +77,7 @@ RSpec.describe "Users", type: :system do
             click_button("Update")
             expect(user_path(user)).to eq(current_path)
             expect(page).to have_content "User was successfully updated."
+            expect(page).to have_content User.first.email
           end
         end
 
