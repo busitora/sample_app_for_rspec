e406a213f78aff613ff6de421161cba55a009800
busitora <ts12272530@gmail.com>
2019-11-07 12:33:25 +0900

    [remove]macroにテストケースを書いていたので削除した

--- spec/support/login_macros.rb
+++ spec/support/login_macros.rb
@@ -4,6 +4,5 @@ module LoginMacros
     fill_in "Email", with: user.email
     fill_in "Password", with: "password"
     click_button("Login")
-    expect(page).to have_content "Login successful"
   end
 end
